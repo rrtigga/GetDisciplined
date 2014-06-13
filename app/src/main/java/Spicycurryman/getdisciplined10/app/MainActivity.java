ce8e6f94f23b6997a879eae835610f2e2c57b27f
Rohit Tigga <rrtigga@ucdavis.edu>
2014-06-13 09:56:20 -0700

    Deleted String

--- app/src/main/java/Spicycurryman/getdisciplined10/app/MainActivity.java
+++ app/src/main/java/Spicycurryman/getdisciplined10/app/MainActivity.java
@@ -1,36 +1,172 @@
 package Spicycurryman.getdisciplined10.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+import com.triggertrap.seekarc.SeekArc;
+import com.triggertrap.seekarc.SeekArc.OnSeekArcChangeListener;
+
+public class MainActivity extends ListActivity {
+    private SeekArc mSeekArc;
+    private SeekBar mRotation;
+    private SeekBar mStartAngle;
+    private SeekBar mSweepAngle;
+    private SeekBar mArcWidth;
+    private SeekBar mProgressWidth;
+    private CheckBox mRoundedEdges;
+    private CheckBox mTouchInside;
+    private CheckBox mClockwise;
+    private TextView mSeekArcProgress;
 
 
-public class MainActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSeekArc = (SeekArc) findViewById(R.id.seekArc);
+        mSeekArcProgress = (TextView) findViewById(R.id.seekArcProgress);
+        mRotation = (SeekBar) findViewById(R.id.rotation);
+        mStartAngle = (SeekBar) findViewById(R.id.startAngle);
+        mSweepAngle  = (SeekBar) findViewById(R.id.sweepAngle);
+        mArcWidth = (SeekBar) findViewById(R.id.arcWidth);
+        mProgressWidth = (SeekBar) findViewById(R.id.progressWidth);
+        mRoundedEdges = (CheckBox) findViewById(R.id.roundedEdges);
+        mTouchInside = (CheckBox) findViewById(R.id.touchInside);
+        mClockwise = (CheckBox) findViewById(R.id.clockwise);
+
+        mRotation.setProgress(mSeekArc.getArcRotation());
+        mStartAngle.setProgress(mSeekArc.getStartAngle());
+        mSweepAngle.setProgress(mSeekArc.getSweepAngle());
+        mArcWidth.setProgress(mSeekArc.getArcWidth());
+        mProgressWidth.setProgress(mSeekArc.getProgressWidth());
+
+        mSeekArc.setOnSeekArcChangeListener(new OnSeekArcChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekArc seekArc) {
+            }
+            @Override
+            public void onStartTrackingTouch(SeekArc seekArc) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekArc seekArc, int progress,
+                                          boolean fromUser) {
+                mSeekArcProgress.setText(String.valueOf(progress));
+            }
+        });
+
+        mRotation.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar view, int progress, boolean fromUser) {
+                mSeekArc.setArcRotation(progress);
+                mSeekArc.invalidate();
+            }
+        });
+
+        mStartAngle.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar view, int progress, boolean fromUser) {
+                mSeekArc.setStartAngle(progress);
+                mSeekArc.invalidate();
+            }
+        });
+
+        mSweepAngle.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar view, int progress, boolean fromUser) {
+                mSeekArc.setSweepAngle(progress);
+                mSeekArc.invalidate();
+            }
+        });
+
+        mArcWidth.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar view, int progress, boolean fromUser) {
+                mSeekArc.setArcWidth(progress);
+                mSeekArc.invalidate();
+            }
+        });
+
+        mProgressWidth.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar view, int progress, boolean fromUser) {
+                mSeekArc.setProgressWidth(progress);
+                mSeekArc.invalidate();
+            }
+        });
+
+        mRoundedEdges.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                mSeekArc.setRoundedEdges(isChecked);
+                mSeekArc.invalidate();
+            }
+        });
+
+        mTouchInside.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                mSeekArc.setTouchInSide(isChecked);
+            }
+        });
+
+        mClockwise.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                mSeekArc.setClockwise(isChecked);
+                mSeekArc.invalidate();
+            }
+        });
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
+
