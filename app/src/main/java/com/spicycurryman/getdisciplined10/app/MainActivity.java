be0fae5481fa6a30c942680ef52967f42903cde4
Rohit Tigga <rrtigga@ucdavis.edu>
2014-06-14 11:52:03 -0700

    -Moved Buttons the the bottom and created merge.xml for the main homescreen -Changed color of buttons

--- app/src/main/java/com/spicycurryman/getdisciplined10/app/MainActivity.java
+++ app/src/main/java/com/spicycurryman/getdisciplined10/app/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.merge);
         mSeekArc = (SeekArc) findViewById(R.id.seekArc);
         mSeekArcProgress = (TextView) findViewById(R.id.seekArcProgress);
         mRotation = (SeekBar) findViewById(R.id.rotation);
