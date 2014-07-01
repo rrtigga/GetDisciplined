1a5e6f3d3a1bac0adcdb30db20c4e9ae8d016d54
Rohit Tigga <rrtigga@ucdavis.edu>
2014-07-01 01:44:18 -0700

    -Changed Main UI

--- app/src/main/java/com/spicycurryman/getdisciplined10/app/MainActivity.java
+++ app/src/main/java/com/spicycurryman/getdisciplined10/app/MainActivity.java
@@ -1,21 +1,24 @@
 package com.spicycurryman.getdisciplined10.app;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
-import android.view.View.OnClickListener;
 
 import com.triggertrap.seekarc.SeekArc;
 import com.triggertrap.seekarc.SeekArc.OnSeekArcChangeListener;
@@ -33,10 +36,17 @@ public class MainActivity extends ActionBarActivity {
     private TextView mSeekArcProgress;
 
     private Button block_button_text;
+    private Button start_timer_text;
     private Button security_settings_button_text;
     private Button blacklist_whitelist_button_text;
     private TextView hour_text;
     private TextView number_text;
+
+    private TextView little_hour_text;
+    private TextView little_minute_text;
+    private TextView little_second_text;
+
+
 
     Button block_button1,security_settings_button1,blacklist_whitelist_button1;
 
@@ -53,24 +63,36 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
+
+
+        SpannableString s = new SpannableString("GetDisciplined");
+        s.setSpan(new TypefaceSpan(this, "roboto-blackitalic.ttf"), 0, s.length(),
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+// Update the action bar title with the TypefaceSpan instance
+        ActionBar actionBar = getActionBar();
+        actionBar.setTitle(s);
         setContentView(R.layout.merge);
 
         //Listeners for the three buttons
         addListenerOnButton();
-        addListenerOnButton2();
-        addListenerOnButton3();
+//        addListenerOnButton2();
+        //addListenerOnButton3();
 
 
         //Editing Button Text
 
         block_button_text = (Button)findViewById(R.id.block_button);
-        block_button_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/robotocondensed-light.ttf"));
+        block_button_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/roboto-light.ttf"));
 
+        start_timer_text = (Button)findViewById(R.id.start_button);
+        start_timer_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/roboto-light.ttf"));
+/*
         security_settings_button_text = (Button)findViewById(R.id.security_settings_button);
-        security_settings_button_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/robotocondensed-light.ttf"));
+        security_settings_button_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/robotocondensed-light.ttf"));*/
 
-        blacklist_whitelist_button_text = (Button)findViewById(R.id.blacklist_whitelist_button);
-        blacklist_whitelist_button_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/robotocondensed-light.ttf"));
+      /*  blacklist_whitelist_button_text = (Button)findViewById(R.id.blacklist_whitelist_button);
+        blacklist_whitelist_button_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/robotocondensed-light.ttf"));*/
 
         hour_text = (TextView)findViewById(R.id.time_text);
         hour_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/roboto-light.ttf"));
@@ -79,11 +101,18 @@ public class MainActivity extends ActionBarActivity {
         number_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/roboto-light.ttf"));
 
 
-        block_button1 = (Button)findViewById(R.id.block_button);
+      /*  block_button1 = (Button)findViewById(R.id.block_button);
         security_settings_button1 = (Button)findViewById(R.id.security_settings_button);
         blacklist_whitelist_button1= (Button)findViewById(R.id.blacklist_whitelist_button);
+*/
+        little_hour_text = (TextView)findViewById(R.id.hourtext);
+        little_hour_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/roboto-light.ttf"));
 
+        little_minute_text = (TextView)findViewById(R.id.minutetext);
+        little_minute_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/roboto-light.ttf"));
 
+        little_second_text = (TextView)findViewById(R.id.secondtext);
+        little_second_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/roboto-light.ttf"));
 
 
 
@@ -252,7 +281,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     // Make sure that you change the intent and class
-    public void addListenerOnButton2() {
+/*    public void addListenerOnButton2() {
 
         final Context context = this;
 
@@ -270,10 +299,10 @@ public class MainActivity extends ActionBarActivity {
 
         });
 
-    }
+    }*/
     // Make sure that you change the intent and class
 
-    public void addListenerOnButton3() {
+/*    public void addListenerOnButton3() {
 
         final Context context = this;
 
@@ -291,7 +320,7 @@ public class MainActivity extends ActionBarActivity {
 
         });
 
-    }
+    }*/
 
 
 
---
+++ app/src/main/java/com/spicycurryman/getdisciplined10/app/TypefaceSpan.java
@@ -0,0 +1,56 @@
+package com.spicycurryman.getdisciplined10.app;
+
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.text.TextPaint;
+import android.text.style.MetricAffectingSpan;
+import android.util.LruCache;
+
+/**
+ * Style a {@linkSpannable} with a custom {@link Typeface}.
+ *
+ * @author Tristan Waddington
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
+public class TypefaceSpan extends MetricAffectingSpan {
+    /** An <code>LruCache</code> for previously loaded typefaces. */
+    private static LruCache<String, Typeface> sTypefaceCache =
+            new LruCache<String, Typeface>(12);
+
+    private Typeface mTypeface;
+
+    /**
+     * Load the {@link Typeface} and apply to a {@linkSpannable}.
+     */
+    public TypefaceSpan(Context context, String typefaceName) {
+        mTypeface = sTypefaceCache.get(typefaceName);
+
+        if (mTypeface == null) {
+            mTypeface = Typeface.createFromAsset(context.getApplicationContext()
+                    .getAssets(), String.format("fonts/roboto-blackitalic.ttf", typefaceName));
+
+            // Cache the loaded Typeface
+            sTypefaceCache.put(typefaceName, mTypeface);
+        }
+    }
+
+    @Override
+    public void updateMeasureState(TextPaint p) {
+        p.setTypeface(mTypeface);
+
+        // Note: This flag is required for proper typeface rendering
+        p.setFlags(p.getFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+    }
+
+    @Override
+    public void updateDrawState(TextPaint tp) {
+        tp.setTypeface(mTypeface);
+
+        // Note: This flag is required for proper typeface rendering
+        tp.setFlags(tp.getFlags() | Paint.SUBPIXEL_TEXT_FLAG);
+    }
+}
