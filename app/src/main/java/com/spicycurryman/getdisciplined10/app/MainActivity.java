2f78b56affcd639d0ba90906ab6fe9264b1b4c3c
rrtigga <rrtigga@ucdavis.edu>
2014-08-15 01:06:21 -0700

    Added ugly alert dialog

--- app/src/main/java/com/spicycurryman/getdisciplined10/app/MainActivity.java
+++ app/src/main/java/com/spicycurryman/getdisciplined10/app/MainActivity.java
@@ -1,7 +1,9 @@
 package com.spicycurryman.getdisciplined10.app;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
@@ -470,6 +472,10 @@ public class MainActivity extends ActionBarActivity {
 
     private void setActionListeners() {
 
+
+
+
+
         number_text = (TextView) findViewById(R.id.hour_progress_number);
         minute_text = (TextView) findViewById(R.id.minute_progress_number);
         second_text = (TextView) findViewById(R.id.second_progress_number);
@@ -478,81 +484,101 @@ public class MainActivity extends ActionBarActivity {
 
 
         start_timer.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View view) {
 
+                new AlertDialog.Builder( MainActivity.this )
+                        .setMessage( "Are you sure you want to block the selected apps?" )
+                        .setPositiveButton( "Yeah man!", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.d( "AlertDialog", "Positive" );
+
+                                hourint = Integer.valueOf(number_text.getText().toString());
+
+                                minuteint = Integer.valueOf(minute_text.getText().toString());
+
+                                secondint = Integer.valueOf(second_text.getText().toString());
+
+                                Log.i("YourActivity", "Hours: " + hourint);
+
+                                Log.i("YourActivity", "Minutes: " + minuteint);
+
+                                Log.i("YourActivity", "Seconds: " + secondint);
+
+                                totalTimeCountInMilliseconds = ((hourint*60*60) +(minuteint*60) + (secondint)) * 1000;      // time count for 3 minutes = 180 seconds
+                                timeBlinkInMilliseconds = 30*1000;
+
+                                countDownTimer = new CountDownTimer(totalTimeCountInMilliseconds, 500) {
+                                    // 500 means, onTick function will be called at every 500 milliseconds
+
+                                    @Override
+                                    public void onTick(long leftTimeInMilliseconds) {
+                                        long seconds = leftTimeInMilliseconds / 1000;
+                                        mSeekArc.setVisibility(View.INVISIBLE);
+                                        start_timer.setVisibility(View.INVISIBLE);
+                                        block_button1.setVisibility(View.INVISIBLE);
+
+
+
+                                        if ( leftTimeInMilliseconds < timeBlinkInMilliseconds ) {
+                                            // textViewShowTime.setTextAppearance(getApplicationContext(), R.style.blinkText);
+                                            // change the style of the textview .. giving a red alert style
+
+                                            if ( blink ) {
+                                                number_text.setVisibility(View.VISIBLE);
+                                                minute_text.setVisibility(View.VISIBLE);
+                                                second_text.setVisibility(View.VISIBLE);
+
+
+                                                // if blink is true, textview will be visible
+                                            } else {
+                                                number_text.setVisibility(View.INVISIBLE);
+                                                minute_text.setVisibility(View.INVISIBLE);
+                                                second_text.setVisibility(View.INVISIBLE);
+
+
+                                            }
+
+                                            blink = !blink;         // toggle the value of blink
+                                        }
+
+                                        second_text.setText(String.format("%02d", seconds % 60));
+                                        minute_text.setText(String.format("%02d", (seconds / 60) % 60));
+                                        number_text.setText(String.format("%02d", seconds / 3600));                     // format the textview to show the easily readable format
+                                    }
+
+
+                                    @Override
+                                    public void onFinish() {
+                                        // this function will be called when the timecount is finished
+                                        //textViewShowTime.setText("Time up!");
+                                        number_text.setVisibility(View.VISIBLE);
+                                        minute_text.setVisibility(View.VISIBLE);
+                                        second_text.setVisibility(View.VISIBLE);
+                                        mSeekArc.setVisibility(View.VISIBLE);
+                                        start_timer.setVisibility(View.VISIBLE);
+                                        block_button1.setVisibility(View.VISIBLE);
+
+
+                                    }
+
+                                }.start();
+                            }
+                        })
+                        .setNegativeButton( "Whoops! No I didn't!", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.d( "AlertDialog", "Negative" );
+                                dialog.cancel();
+                            }
+                        } )
+                        .show();
+
+
+
                // textViewShowTime.setTextAppearance(getApplicationContext(), R.style.normalText);
 
-                hourint = Integer.valueOf(number_text.getText().toString());
-
-                minuteint = Integer.valueOf(minute_text.getText().toString());
-
-                secondint = Integer.valueOf(second_text.getText().toString());
-
-                Log.i("YourActivity", "Hours: " + hourint);
-
-                Log.i("YourActivity", "Minutes: " + minuteint);
-
-                Log.i("YourActivity", "Seconds: " + secondint);
-
-                totalTimeCountInMilliseconds = ((hourint*60*60) +(minuteint*60) + (secondint)) * 1000;      // time count for 3 minutes = 180 seconds
-                timeBlinkInMilliseconds = 30*1000;
-
-                countDownTimer = new CountDownTimer(totalTimeCountInMilliseconds, 500) {
-                    // 500 means, onTick function will be called at every 500 milliseconds
-
-                    @Override
-                    public void onTick(long leftTimeInMilliseconds) {
-                        long seconds = leftTimeInMilliseconds / 1000;
-                        mSeekArc.setVisibility(View.INVISIBLE);
-                        start_timer.setVisibility(View.INVISIBLE);
-                        block_button1.setVisibility(View.INVISIBLE);
-
-
-
-                        if ( leftTimeInMilliseconds < timeBlinkInMilliseconds ) {
-                           // textViewShowTime.setTextAppearance(getApplicationContext(), R.style.blinkText);
-                            // change the style of the textview .. giving a red alert style
-
-                            if ( blink ) {
-                                number_text.setVisibility(View.VISIBLE);
-                                minute_text.setVisibility(View.VISIBLE);
-                                second_text.setVisibility(View.VISIBLE);
-
-
-                                // if blink is true, textview will be visible
-                            } else {
-                                number_text.setVisibility(View.INVISIBLE);
-                                minute_text.setVisibility(View.INVISIBLE);
-                                second_text.setVisibility(View.INVISIBLE);
-
-
-                            }
-
-                            blink = !blink;         // toggle the value of blink
-                        }
-
-                        second_text.setText(String.format("%02d", seconds % 60));
-                        minute_text.setText(String.format("%02d", (seconds / 60) % 60));
-                        number_text.setText(String.format("%02d", seconds / 3600));                     // format the textview to show the easily readable format
-                    }
-
-
-                    @Override
-                    public void onFinish() {
-                        // this function will be called when the timecount is finished
-                        //textViewShowTime.setText("Time up!");
-                        number_text.setVisibility(View.VISIBLE);
-                        minute_text.setVisibility(View.VISIBLE);
-                        second_text.setVisibility(View.VISIBLE);
-                        mSeekArc.setVisibility(View.VISIBLE);
-                        start_timer.setVisibility(View.VISIBLE);
-                        block_button1.setVisibility(View.VISIBLE);
-
-
-                    }
-
-                }.start();
 
             }
         });
