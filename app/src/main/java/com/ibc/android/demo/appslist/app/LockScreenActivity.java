2d0b93818957de6ba4855ec0a1e60e0f0d996a8e
rrtigga <rrtigga@ucdavis.edu>
2014-08-29 01:27:37 -0700

    -THANK YOU JESUS! YOU SHOWED ME THE CODE AND THE COMPILER DIDN'T BREAK ME DOWN!

--- app/src/main/java/com/ibc/android/demo/appslist/app/LockScreenActivity.java
+++ app/src/main/java/com/ibc/android/demo/appslist/app/LockScreenActivity.java
@@ -110,4 +110,89 @@ public class LockScreenActivity extends Activity {
         super.onBackPressed();
     }
 
-}
+
+
+    @Override
+    public void onStop(){
+
+
+        /*
+         * So here's what's going on: When the user presses the back button,
+         * the Instagram app (or any other app that's being blocked) is relaunched
+         * from the stack and that triggers the LockScreenActivity to be launched
+         * again. Kinda goes into an infinite loop in a sense. What I'm going to do
+         * to get around that, is completely kill the Instagram application.
+         */
+
+
+
+        /*
+         * Every running application has a PID (process id). This is how the system
+         * keeps track of which apps are running. First, we're gonna need to find the
+         * PID of the blocked app (Instagram in this case).
+         */
+
+            // Grab a list of all running processes and their PIDs.
+            ActivityManager am = (ActivityManager) getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+            List<ActivityManager.RunningAppProcessInfo> pids = am.getRunningAppProcesses();
+
+            // Now loop through the list of PIDs and find Instagram's PID.
+            sharedPrefsapp = getApplicationContext().getSharedPreferences("appdb", Context.MODE_PRIVATE);
+            allEntries= null;
+            allEntries = sharedPrefsapp.getAll();
+
+            //prefix = "m";
+            packagezList= null;
+
+
+            packagezList = new ArrayList<String>();
+
+
+
+
+            for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+                //Log.e("right key: ", entry.getKey() + "right value: " + entry.getValue().toString()  );
+                packagezList.add(entry.getKey());
+            }
+
+
+            // Killing any process for blocked applications when the back button is pressed while the lock screen is displayed
+
+            for(Object object: packagezList){
+                am.killBackgroundProcesses((String) object);
+                Log.d("Killed Background Process!: ", (String) object);
+
+
+            }
+
+
+
+            // Now that we've got the PID, kill the Instagram process.
+
+
+            // Now that we've got the PID, kill the Instagram process.
+
+            ActivityManager  am1 = (ActivityManager) getApplicationContext().getSystemService(ACTIVITY_SERVICE);
+
+
+            // Display confirmation here, finish() activity.
+            Intent startMain = new Intent(Intent.ACTION_MAIN);
+            startMain.addCategory(Intent.CATEGORY_HOME);
+            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(startMain);
+
+
+            startService(new Intent(this, HeartBeat.class));
+
+
+  /*      Intent iHeartBeatService = new Intent(this, HeartBeat.class);
+        PendingIntent piHeartBeatService = PendingIntent.getService(this, 0, iHeartBeatService, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(piHeartBeatService);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 300000, piHeartBeatService);*/
+
+            finish();
+            super.onStop();
+        }
+    }
+
